10c776ee148e5ebf8d262e003bc5a7b52abd1afc
Mere Daniel <support@brandonetech.com>
2022-06-20 08:47:18 +0100

    Presentation

--- Backend_UMR_Work_Program/Controllers/PresentationScheduleController.cs
+++ Backend_UMR_Work_Program/Controllers/PresentationScheduleController.cs
@@ -165,11 +165,7 @@ namespace Backend_UMR_Work_Program.Controllers
             return new WebApiResponse { ResponseCode = AppResponseCodes.Success, Message = "Success", Data = details.OrderBy(x => x.YEAR), StatusCode = ResponseCodes.Success };
 
         }
-        [HttpGet("COMPANY_REPS")]
-        public async Task<object> Get_Company_Replist()
-        {
-            return await _context.ADMIN_DIVISION_REP_LISTs.ToListAsync();         
-        }
+       
         [HttpGet("GET_COMPANY_REP")]
         public async Task<WebApiResponse> Comprep(int Id)
         {
@@ -199,159 +195,9 @@ namespace Backend_UMR_Work_Program.Controllers
         }
        
 
-        [HttpPost("UPDATE_COMPANY_REP")]
-        public async Task<WebApiResponse> UpdateRep(int compId, [FromBody] Admin_Reps model)
-        {
-            if (UserWorkRole == GeneralModel.Admin)
-            {
-                try
-                {
-                    int save = 0;
-                    var _comp = _context.ADMIN_DIVISIONAL_REPS_PRESENTATIONs.FirstOrDefault(n => n.Id == compId);
-                    if (_comp != null)
-                    {
-                        _comp.REPRESENTATIVE = model.REPRESENTATIVE;
-                        _comp.REPRESENTATIVE_EMAIL = model.REPRESENTATIVE_EMAIL;
-                        _comp.Date_Updated = DateTime.Now;
-
-                        save = _context.SaveChanges();
-                    }
-                    if (save > 0)
-                    {
-                        string message = "Company representative updated sucessfully";
-                        return new WebApiResponse { ResponseCode = AppResponseCodes.Success, Message = message, Data = _comp, StatusCode = ResponseCodes.Success };
-                    }
-                }
-                catch (Exception ex)
-                {
-                    return new WebApiResponse { ResponseCode = AppResponseCodes.UserNotFound, Message = "Error occured", StatusCode = ResponseCodes.RecordNotFound };
-
-                }
-            }
-            return new WebApiResponse { ResponseCode = AppResponseCodes.UserNotFound, Message = "No data found", StatusCode = ResponseCodes.RecordNotFound };
-        }
         
-        [HttpPost("ACTIVATE_DEACTIVATE_EMAIL")]
-        public async Task<WebApiResponse> UpdateEmailStatus( int compId, string option)
-        {
-            string optionA = "Active";
-            string optionB = "Inactive";
-            string active = "Successful";
-            string inactive = "Inactive";
-            int save = 0;
-            var dateTimeId = _context.ADMIN_DATETIME_PRESENTATIONs.FirstOrDefault(x => x.Id == compId);
-            if(dateTimeId.EMAIL_REMARK == null)
-            {
-                dateTimeId.EMAIL_REMARK = inactive;
-            }
-            if(UserWorkRole == GeneralModel.Admin)
-            {
-                try
-                {
-                    if(dateTimeId != null)
-                    {
-                        if (dateTimeId != null && option == optionA && dateTimeId.EMAIL_REMARK.ToLower() != active.ToLower())
-                        {
-                            dateTimeId.EMAIL_REMARK = active;
-
-                        }
-                        if (dateTimeId != null && option == optionB && dateTimeId.EMAIL_REMARK.ToLower() != inactive.ToLower())
-                        {
-                            dateTimeId.EMAIL_REMARK = inactive.ToUpper();
-
-                        }
-                        save = _context.SaveChanges();
-                        if (save > 0)
-                        {
-                            string message = "Company admin representative updated sucessfully";
-                            return new WebApiResponse { ResponseCode = AppResponseCodes.Success, Message = message, Data = compId, StatusCode = ResponseCodes.Success };
-                        }
-                    }
-                }
-                catch (Exception)
-                {
-
-                    return new WebApiResponse { ResponseCode = AppResponseCodes.UserNotFound, Message = "Error Occured", StatusCode = ResponseCodes.RecordNotFound };
-
-                }
-
-            }
-            return new WebApiResponse { ResponseCode = AppResponseCodes.UserNotFound, Message = "Not Successful", StatusCode = ResponseCodes.RecordNotFound };
-
-        }
-        [HttpPost("UPLOAD_MOM")]
-        public async Task<WebApiResponse> UPLOADMOM(int compId, IFormFile document)
-        {
-            //var userRole = _helpersController.Decrypt(_httpContextAccessor.HttpContext.Session.GetString(Authentications.AuthController.sessionRoleName));
-            //var userEmail = _helpersController.Decrypt(_httpContextAccessor.HttpContext.Session.GetString(Authentications.AuthController.sessionEmail));
-            //var companyID = _helpersController.Decrypt(_httpContextAccessor.HttpContext.Session.GetString(Authentications.AuthController.sessionUserID));
-            
-            var userRole = "Admin";
-            var userName = "testname";
-            var userEmail = "test@mailinator.com";
-            var companyID = "NND/001";
-            var CurrentYear = DateTime.Now.Year.ToString();
-            string folderToSave = "";
-            var checkUploadedMom = _context.ADMIN_DATETIME_PRESENTATIONs.Where(c => c.Id == compId).FirstOrDefault();
-
-            if (checkUploadedMom.MOM_UPLOAD_DATE != null)
-            {
-                //schedule already exist for company
-                return new WebApiResponse { ResponseCode = AppResponseCodes.Failed, Message = "You have already uploaded minutes for the selected year, kindly delete an existing document to upload another.", StatusCode = ResponseCodes.Failure };
-            }else
-            {
-                //check if date has been scheduled by another company
-                var system_date = DateTime.Now.ToString(); // 
-
-                var uniqueFileName = "";
-                var FileExtension = "";
-                var newFileName = "";
-                var documentPath = "";
-                var fileName = "";
-                var save = 0;
-                //For image cover
-                if (document != null)
-                {
-                    if (document.Length > 0)
-                    {
-                        var up = Path.Combine(Directory.GetCurrentDirectory(), "Documents");
-                        var uploads = Path.Combine(up, "UploadMOMs");
-                        fileName = document.FileName.Split(".")[0].Trim();
-                        uniqueFileName = Convert.ToString(Guid.NewGuid());
-                        FileExtension = document.FileName.Split(".")[1].Trim();
-                        
-                        newFileName = fileName + "." + FileExtension;
-
-                        
-                        documentPath = "//Documents/UploadMOMs/" + newFileName;
-                        using (var s = new FileStream(Path.Combine(uploads, newFileName),
-                             FileMode.Create))
-                        {
-                            document.CopyTo(s);
-                        }
-                    }
-                    checkUploadedMom.MOM = documentPath;
-                    checkUploadedMom.MOM_UPLOADED_BY = userEmail;
-                    checkUploadedMom.MOM_UPLOAD_DATE = DateTime.Now;
-                    save = _context.SaveChanges();
-                    if (save > 0)
-                    {
-                        var companyUploadedmom = _context.ADMIN_DATETIME_PRESENTATIONs.Where(x => x.Id == compId).ToList();
-
-                        return new WebApiResponse { ResponseCode = AppResponseCodes.Success, Message = "File uploaded successfully.", Data = companyUploadedmom, StatusCode = ResponseCodes.Success };
-                    }
-                    else
-                    {
-                        return new WebApiResponse { ResponseCode = AppResponseCodes.Failed, Message = "An error occured while trying to upload this minutes.", StatusCode = ResponseCodes.Failure };
-
-                    }
-                }
-                else
-                {
-                    return new WebApiResponse { ResponseCode = AppResponseCodes.Failed, Message = "Sorry, document is empty.", StatusCode = ResponseCodes.Failure };
-
-                }
-            }          
-        }
+        
+        
+       
     }
 }
