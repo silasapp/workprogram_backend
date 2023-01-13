0b553a59c8247c7ff84a3e72a918902e619f6e02
jovita69 <nwanaforoc@gmail.com>
2023-01-13 16:22:56 +0100

    Operation Safety Case done

--- Backend_UMR_Work_Program/DataModels/HSE_TECHNICAL_SAFETY_CONTROL_STUDIES_NEW.cs
+++ Backend_UMR_Work_Program/DataModels/HSE_TECHNICAL_SAFETY_CONTROL_STUDIES_NEW.cs
@@ -1,30 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Backend_UMR_Work_Program.Models
+﻿namespace Backend_UMR_Work_Program.Models
 {
-    public partial class HSE_TECHNICAL_SAFETY_CONTROL_STUDIES_NEW
-    {
-        public int Id { get; set; }
-        public string? OML_ID { get; set; }
-        public string? OML_Name { get; set; }
-        public string? CompanyName { get; set; }
-        public string? Companyemail { get; set; }
-        public string? Year_of_WP { get; set; }
-        public string? facility { get; set; }
-        public string? study_type { get; set; }
-        public string? facility_location { get; set; }
-        public string? approval_status { get; set; }
-        public string? remarks { get; set; }
-        public string? Created_by { get; set; }
-        public string? Updated_by { get; set; }
-        public DateTime? Date_Created { get; set; }
-        public DateTime? Date_Updated { get; set; }
-        public string? Contract_Type { get; set; }
-        public string? Terrain { get; set; }
-        public string? Consession_Type { get; set; }
-        public string? COMPANY_ID { get; set; }
-        public int? CompanyNumber { get; set; }
-        public int? Field_ID { get; set; }
-    }
+	public partial class HSE_TECHNICAL_SAFETY_CONTROL_STUDIES_NEW
+	{
+		public int Id { get; set; }
+		public string? OML_ID { get; set; }
+		public string? OML_Name { get; set; }
+		public string? CompanyName { get; set; }
+		public string? Companyemail { get; set; }
+		public string? Year_of_WP { get; set; }
+		public string? facility { get; set; }
+		public string? study_type { get; set; }
+		public string? facility_location { get; set; }
+		public string? approval_status { get; set; }
+		public string? remarks { get; set; }
+		public string? Created_by { get; set; }
+		public string? Updated_by { get; set; }
+		public DateTime? Date_Created { get; set; }
+		public DateTime? Date_Updated { get; set; }
+		public string? Contract_Type { get; set; }
+		public string? Terrain { get; set; }
+		public string? Consession_Type { get; set; }
+		public string? COMPANY_ID { get; set; }
+		public int? CompanyNumber { get; set; }
+		public int? Field_ID { get; set; }
+		public string? Type_Of_Facility { get; set; }
+		public string? Number_of_Facilities { get; set; }
+	}
 }
