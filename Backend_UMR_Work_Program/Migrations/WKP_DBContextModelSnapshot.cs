869e865c967cacab44342702f28550dc3ef76a16
frank <silas.o@targetafrica.net>
2023-02-02 08:02:35 +0100

    099

--- Backend_UMR_Work_Program/Migrations/WKP_DBContextModelSnapshot.cs
+++ Backend_UMR_Work_Program/Migrations/WKP_DBContextModelSnapshot.cs
@@ -356,13 +356,9 @@ namespace Backend_UMR_Work_Program.Migrations
                         .HasColumnType("varchar(200)");
 
                     b.Property<string>("CompanyId")
-<<<<<<< HEAD
-                        .HasColumnType("nvarchar(max)");
-=======
                         .HasMaxLength(50)
                         .IsUnicode(false)
                         .HasColumnType("varchar(50)");
->>>>>>> ae6acd990212a2071c9cd43d1fadc65d5e80a4e6
 
                     b.Property<int?>("CompanyNumber")
                         .HasColumnType("int");
@@ -14083,12 +14079,9 @@ namespace Backend_UMR_Work_Program.Migrations
                     b.Property<string>("Evidence_of_pay_of_DDCPath")
                         .HasColumnType("nvarchar(max)");
 
-<<<<<<< HEAD
-=======
                     b.Property<string>("Field_Name")
                         .HasColumnType("nvarchar(max)");
 
->>>>>>> ae6acd990212a2071c9cd43d1fadc65d5e80a4e6
                     b.Property<string>("OML_ID")
                         .HasColumnType("nvarchar(max)");
 
@@ -16235,13 +16228,9 @@ namespace Backend_UMR_Work_Program.Migrations
                         .HasColumnType("varchar(100)");
 
                     b.Property<string>("Year")
-<<<<<<< HEAD
-                        .HasColumnType("nvarchar(max)");
-=======
                         .HasMaxLength(1000)
                         .IsUnicode(false)
                         .HasColumnType("varchar(1000)");
->>>>>>> ae6acd990212a2071c9cd43d1fadc65d5e80a4e6
 
                     b.Property<string>("Year_of_WP")
                         .HasMaxLength(100)
@@ -20127,16 +20116,8 @@ namespace Backend_UMR_Work_Program.Migrations
                         .HasColumnType("varchar(100)");
 
                     b.Property<string>("Last_Qntr_Royalty")
-<<<<<<< HEAD
-                        .HasColumnType("nvarchar(max)");
-=======
-                        .HasMaxLength(1000)
-                        .IsUnicode(false)
-                        .HasColumnType("varchar(1000)");
->>>>>>> ae6acd990212a2071c9cd43d1fadc65d5e80a4e6
 
                     b.Property<string>("Miscellaneous")
-                        .HasMaxLength(100)
                         .IsUnicode(false)
                         .HasColumnType("varchar(100)");
 
