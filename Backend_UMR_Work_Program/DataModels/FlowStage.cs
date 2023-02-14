bf3c27bfafad06d6a1c3e01ee3359a4d975469f5
jovita69 <nwanaforoc@gmail.com>
2023-02-14 10:28:03 +0100

    Process Updated

---
+++ Backend_UMR_Work_Program/DataModels/FlowStage.cs
@@ -0,0 +1,15 @@
+﻿namespace Backend_UMR_Work_Program.DataModels
+{
+	public class FlowStage
+	{
+		public int Id { get; set; }
+		public string? Action { get; set; }
+		public int? SBUId { get; set; }
+		public string? Status { get; set; }
+		public int? TargetedRole { get; set; }
+		public int? TargetedSBU { get; set; }
+		public bool? IsArchived { get; set; }
+		public int? TriggeredByRole { get; set; }
+		public int? TriggeredBySBU { get; set; }
+	}
+}
---
+++ Backend_UMR_Work_Program/Models/ProcessFlowModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend_UMR_Work_Program.Models
+{
+	public class ProcessFlowModel
+	{
+		public List<int?> TriggeredBySUBId { get; set; }
+		public List<int?> TriggeredByRoleId { get; set; }
+		public List<int?> TargetedBySUBId { get; set; }
+		public List<int?> TargetedByRoleId { get; set; }
+
+
+	}
+}
