b1784f8b460264525efb64bd0e1c290e123e658b
TargetAfrica <admin@targetafrica.net>
2023-02-15 13:12:06 +0100

    Deploy 1483

--- Backend_UMR_Work_Program/Migrations/20230215115526_ToInt.Designer.cs
+++ Backend_UMR_Work_Program/Migrations/20230215115526_ToInt.Designer.cs
@@ -4,6 +4,7 @@ using Backend_UMR_Work_Program.DataModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BackendUMRWorkProgram.Migrations
 {
     [DbContext(typeof(WKP_DBContext))]
-    partial class WKPDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230215115526_ToInt")]
+    partial class ToInt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
