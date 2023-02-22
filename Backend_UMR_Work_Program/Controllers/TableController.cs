b8b5ba5cc8565d2a2a44f5ee7217b8eaf318e3b0
TargetAfrica <admin@targetafrica.net>
2023-02-22 11:02:56 +0100

    Deploy 1600

--- Backend_UMR_Work_Program/Controllers/TableController.cs
+++ Backend_UMR_Work_Program/Controllers/TableController.cs
@@ -1,5 +1,8 @@
-﻿using AutoMapper;
+﻿using System.Data;
+using System.Data.SqlClient;
+using AutoMapper;
 using Backend_UMR_Work_Program.DataModels;
+using ClosedXML.Excel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend_UMR_Work_Program.Controllers
@@ -24,20 +27,51 @@ namespace Backend_UMR_Work_Program.Controllers
 
 
 
-        [HttpPost("REMOVE_USER")]
-        public async Task<int> REMOVE_USER()
+        // [HttpPost("REMOVE_USER")]
+        // public async Task<int> REMOVE_USER()
+        // {
+        //     try
+        //     {
+        //         var data = (from c in _context.ADMIN_COMPANY_INFORMATIONs where c.EMAIL == "damilare.olanrewaju@brandonetech.com" select c).ToList();
+
+        //         _context.RemoveRange(data);
+        //         return await _context.SaveChangesAsync();
+               
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         return 0;
+
+        //     }
+        // }
+
+
+        [HttpPost("ViewTable")]
+        public async Task<FileStreamResult> ViewTable(string tableName)
         {
             try
             {
-                var data = (from c in _context.ADMIN_COMPANY_INFORMATIONs where c.EMAIL == "damilare.olanrewaju@brandonetech.com" select c).ToList();
-
-                _context.RemoveRange(data);
-                return await _context.SaveChangesAsync();
+                using (SqlConnection conn = new SqlConnection("Server=tcp:workprogramdemosvr.database.windows.net,1433;Initial Catalog=WorkProgramdemo;Persist Security Info=False;User ID=WorkProgadmin;Password=*123brandonetech#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+                {
+                    await conn.OpenAsync();
+                    string query0 = $"SELECT * FROM {tableName};";
+                    SqlCommand cmd0 = new SqlCommand(query0, conn);
+                    var reader = await cmd0.ExecuteReaderAsync();
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+                    XLWorkbook wb = new XLWorkbook();
+                    wb.Worksheets.Add(dt, "WKPTable");
+                    Stream fs = new MemoryStream();
+                    wb.SaveAs(fs);
+                    fs.Position = 0;
+                    var file = new FormFile(fs, 0, fs.Length, "name", "WKPTable");
+                    return File(file.OpenReadStream(), file.ContentType, file.Name);
+                }
                
             }
             catch (Exception e)
             {
-                return 0;
+                return null;
 
             }
         }
